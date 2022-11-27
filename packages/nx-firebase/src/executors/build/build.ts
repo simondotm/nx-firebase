2cb4a28a61bc687fe44a8b41475323c7d485e24d
Simon M <simondotm@users.noreply.github.com>
2022-11-27 22:32:19 +0000

    starting on custom build, tests failing

--- packages/nx-firebase/src/executors/build/build.ts
+++ packages/nx-firebase/src/executors/build/build.ts
@@ -1,4 +1,21 @@
-/* 
+import { ExecutorContext, logger } from '@nrwl/devkit'
+import type { ExecutorOptions } from '@nrwl/js/src/utils/schema'
+import { tscExecutor as jsTscExecutor } from '@nrwl/js/src/executors/tsc/tsc.impl'
+
+export async function* runExecutor(
+  options: ExecutorOptions,
+  context: ExecutorContext,
+) {
+  logger.log('running our custom build executor')
+  yield* jsTscExecutor(options, context)
+  logger.log('post yield from jsTscExecutor in our custom build executor')
+}
+
+//export default convertNxExecutor(runExecutor);
+
+export default runExecutor
+
+/*
 import { ExecutorContext } from '@nrwl/devkit'
 import type { NodeExecutorOptions } from '@nrwl/js/src/executors/node/schema'
 import { nodeExecutor as jsNodeExecutor } from '@nrwl/js/src/executors/node/node.impl'
@@ -15,6 +32,7 @@ export async function* runExecutor(
 export default runExecutor
 */
 
+/*
 import { BuildExecutorSchema } from './schema'
 
 export default async function runExecutor(options: BuildExecutorSchema) {
@@ -23,3 +41,4 @@ export default async function runExecutor(options: BuildExecutorSchema) {
     success: true,
   }
 }
+*/
