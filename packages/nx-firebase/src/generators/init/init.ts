7aaa1ceb14101729c0f1c499ac0e63b90c09e98d
Simon M <simondotm@users.noreply.github.com>
2022-11-27 22:31:38 +0000

    add git ignore in init

--- packages/nx-firebase/src/generators/init/init.ts
+++ packages/nx-firebase/src/generators/init/init.ts
@@ -3,22 +3,25 @@ import { convertNxGenerator, formatFiles } from '@nrwl/devkit'
 import { initGenerator as nodeInitGenerator } from '@nrwl/node'
 import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial'
 import { addDependencies, normalizeOptions } from './lib'
+import { addGitIgnoreEntry } from './lib/add-git-ignore-entry'
 import type { InitGeneratorOptions } from './schema'
 
 /**
  * `nx g @simondotm/nx-firebase:init` is based on the `@nrwl/nest` plugin
  *   which in turn is based on the `@nrwl/node` plugin
- * 
+ *
  * Ensures the necessary firebase packages are installed in the nx workspace
  * The `@nrwl/node` init generate also ensures jest configs
- * 
- * Docs say its for internal use only, but nest uses it, so we use it :) 
+ *
+ * Docs say its for internal use only, but nest uses it, so we use it :)
  * https://nx.dev/packages/node/generators/init
  */
 export async function initGenerator(
   tree: Tree,
   rawOptions: InitGeneratorOptions,
 ): Promise<GeneratorCallback> {
+  addGitIgnoreEntry(tree)
+
   const options = normalizeOptions(rawOptions)
   const nodeInitTask = await nodeInitGenerator(tree, options)
   const installPackagesTask = addDependencies(tree)
---
+++ packages/nx-firebase/src/generators/init/lib/add-git-ignore-entry.ts
@@ -0,0 +1,21 @@
+import { logger, Tree } from '@nrwl/devkit'
+
+export const gitIgnoreEntries = `
+# Nx-Firebase
+.runtimeconfig.json
+`
+
+export function addGitIgnoreEntry(host: Tree) {
+  if (!host.exists('.gitignore')) {
+    logger.warn(`Couldn't find .gitignore file to update`)
+    return
+  }
+
+  let content = host.read('.gitignore')?.toString('utf-8').trimEnd()
+
+  if (!/\.expo\/$/gm.test(content)) {
+    content = `${content}\n${gitIgnoreEntries}\n`
+  }
+
+  host.write('.gitignore', content)
+}
