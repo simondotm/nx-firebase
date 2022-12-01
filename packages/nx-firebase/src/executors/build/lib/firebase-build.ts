8831e9c8f5b12e5bd7d5aacbf210488380514202
Simon M <simondotm@users.noreply.github.com>
2022-12-01 00:35:32 +0000

    remove unused code

--- packages/nx-firebase/src/executors/build/lib/firebase-build.ts
+++ packages/nx-firebase/src/executors/build/lib/firebase-build.ts
@@ -1,18 +1,14 @@
-import { ExecutorContext, logger } from '@nrwl/devkit'
+import { ExecutorContext } from '@nrwl/devkit'
 
 import { copyFirebaseDependencies } from './copy-dependencies'
 import { getFirebaseDependencies } from './get-dependencies'
 import { rewriteFirebasePackage } from './rewrite-package'
-import { updateFirebasePackage } from './update-package'
 
 export async function firebaseBuildExecutor(
   context: ExecutorContext,
   outputPath: string,
 ) {
   const firebaseDependencies = getFirebaseDependencies(context)
-  // --updateBuildableProjectDepsInPackageJson is true by default for @nrwl/js:tsc
-  // https://nx.dev/packages/js/executors/tsc
-  // updateFirebasePackage(context, firebaseDependencies)
   const localLibraries = await copyFirebaseDependencies(
     outputPath,
     context.root,
