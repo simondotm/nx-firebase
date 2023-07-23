4bc66d2e0d0efc1e2d1aaa7535fd0419aa6f43cb
Simon M <simondotm@users.noreply.github.com>
2023-07-23 11:20:07 +0100

    Update default function template (#130)
    * Update function main.ts template
    
    * Update versions.ts
    
    * Fix e2e tests

--- e2e/nx-firebase-e2e/test-utils/index.ts
+++ e2e/nx-firebase-e2e/test-utils/index.ts
@@ -162,18 +162,29 @@ export function getProjectData(type: 'libs' | 'apps', name: string, options?: { 
   }
 }
 
-const IMPORT_MATCH = `import * as functions from 'firebase-functions';`
+const IMPORT_MATCH = `import * as logger from "firebase-functions/logger";`
 
 export function getMainTs() {
   return `
-import * as admin from "firebase-admin";
-${IMPORT_MATCH}
-admin.initializeApp();
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+import {onRequest} from "firebase-functions/v2/https";
+${IMPORT_MATCH}
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
-});  
+});
 `
 }
 
