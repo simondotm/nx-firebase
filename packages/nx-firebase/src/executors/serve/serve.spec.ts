aff0f972eed4b153bf720755ff47f4988501b34c
Simon M <simondotm@users.noreply.github.com>
2023-09-03 21:08:47 +0100

    Add custom exector for serve

---
+++ packages/nx-firebase/src/executors/serve/serve.spec.ts
@@ -0,0 +1,11 @@
+// import { FirebaseServeExecutorSchema } from './schema'
+// import runFirebaseServeExecutor from './firebase'
+
+// const options: FirebaseServeExecutorSchema = {}
+
+describe('Firebase Serve Executor', () => {
+  it('can run', async () => {
+    // const output = await runFirebaseServeExecutor([])
+    // expect(output.success).toBe(true)
+  })
+})
