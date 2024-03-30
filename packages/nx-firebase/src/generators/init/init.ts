3028e35a9a883d71adb5e5d1bb8e36aeeffb3f84
Simon M <simondotm@users.noreply.github.com>
2024-03-30 09:09:14 +0000

    Fix lint error

--- packages/nx-firebase/src/generators/init/init.ts
+++ packages/nx-firebase/src/generators/init/init.ts
@@ -4,15 +4,15 @@ import { addGitIgnore, addNxIgnore } from './lib/add-git-ignore-entry'
 import type { InitGeneratorOptions } from './schema'
 
 /**
- * `nx g @simondotm/nx-firebase:init` 
- * 
+ * `nx g @simondotm/nx-firebase:init`
+ *
  * Ensures the necessary firebase packages are installed in the nx workspace
  * It also adds `@nx/node` if it is not already installed
  *
  */
 export async function initGenerator(
   tree: Tree,
-  options: InitGeneratorOptions,
+  options: InitGeneratorOptions,  // eslint-disable-line
 ): Promise<GeneratorCallback> {
   addGitIgnore(tree)
   addNxIgnore(tree)
@@ -20,4 +20,3 @@ export async function initGenerator(
 }
 
 export default initGenerator
-
