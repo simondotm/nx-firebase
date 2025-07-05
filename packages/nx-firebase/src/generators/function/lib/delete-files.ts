63f4e85d66698ab1086a10921a04c0791c8c2f55
KingDarBoja <stevenbojato04@gmail.com>
2025-07-05 13:59:11 -0500

    fix: import at function generator

--- packages/nx-firebase/src/generators/function/lib/delete-files.ts
+++ packages/nx-firebase/src/generators/function/lib/delete-files.ts
@@ -1,13 +1,16 @@
 import type { Tree } from '@nx/devkit'
 import { joinPathFragments } from '@nx/devkit'
-import type { NormalizedSchema } from '../schema'
+import type { FunctionGeneratorNormalizedSchema } from '../schema'
 
 /**
  * Delete unwanted files created by the node generator
  * @param host
  * @param options
  */
-export function deleteFiles(host: Tree, options: NormalizedSchema): void {
+export function deleteFiles(
+  host: Tree,
+  options: FunctionGeneratorNormalizedSchema,
+): void {
   const nodeFilesToDelete = [
     joinPathFragments(options.projectRoot, 'src', 'main.ts'),
     joinPathFragments(options.projectRoot, 'src', 'app', '.gitkeep'),
